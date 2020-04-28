74f401e204f53770e6ef5aaf29e8540978cc911a
Raj Jain <rajjain6201@gmail.com>
2020-04-28 17:48:00 +0530

    webdriver setup

--- src/test/java/library/WebDriverManager.java
+++ src/test/java/library/WebDriverManager.java
@@ -1,5 +1,118 @@
 package library;
 
-public class WebDriverManager {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.LocalFileDetector;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+public class WebDriverManager {
+	
+	private WebDriver webDriver;
+	private DesiredCapabilities capabiities;
+	private EventFiringWebDriver driver;
+	
+	private Boolean isBrowserClosed;
+	
+	//private EventImplementations evenImplementations=new EventImplementations();
+	
+	public void setIsBrowserClosed(final Boolean browserStatus) {
+		this.isBrowserClosed=browserStatus;
+	}
+	
+	public WebDriver getDriver(){
+		if(this.driver!=null)
+			return intializeDriver(Configuration.browser);
+		
+	}
+	
+	public WebDriver initializeDriver(String browser) {
+		try {
+			if(!Configuration.hubAddress.equalsIgnoreCase("LOCAL")) {
+				System.setProperty("java.awt.headless", "false");
+				
+				switch (browser.toUpperCase()) {
+				
+				case "IE":
+				case "INTERNET EXPLORER":
+					capabiities=DesiredCapabilities.internetExplorer();
+					Capabilities.setBrowserName("internet explorer");
+					capabilities.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION,true);
+					capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,true);
+					webDriver = new RemoteWebDriver(new URL(Configuration.hubAddress), options);
+					break;
+					
+				case "CH":
+				case "CHROME":
+					capabilities=DesiredCapabilities.chrome();
+					Capabilities.setBrowserName("chrome");
+					ChromeOptions=new ChromeOptions();
+					options.merge(capabilities);
+					webDriver=new RemoteWebDriver(new URL(Configuratios.hubAddress),options);
+					break;
+							
+				}
+			}else {
+				switch (browser.toUpperCase()) {
+				case "IE":
+				case "INTERNET EXPLORER":
+					System.setProperty("webdriver.ie.driver", Configuration.driverPath + "IEDriverServer.exe");
+					capabilities=DesiredCapabilities.internetExplorer();
+					capabilities.setCapability("unexpectedAlertBehaviour","accept");
+					capabillities.setCapability("ignoreZoomSetting", true);
+					capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,true);
+					InternetExplorerOptions options=new InternetExplorerOptions();
+					options.merge(capabiities);
+					webDriver =new InternetExplorerDriver(options);
+					break;
+					
+				case "CH":
+				case "CHROME":
+					System.setProperty("webdriver.chrome.driver", Configuration.driverPath + "ChromeDriver.exe");
+					ChromeOptions opts=new ChromeOptions();
+					opts.setExperimentalOption("useAutomationExtension", false);
+					webDriver = new ChromeDriver(opts);
+					break;
+					
+				default:
+					Log.info("Please provide IE or Chrome browser in Config file");
+					break;	
+					
+				}
+			}
+			
+			driver=new EventFiringWebDriver(webDriver);
+			driver.register(eventImplementation);
+			driver.manage().window().maximize();
+			((RemoteWebDriver) webDriver).setFileDetector(new LocalFileDetector());
+			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return this.driver;
+	}
+	
+	public void CloseDriver() {
+		try {
+			if(webDriver!=null) {
+				webDriver.quit();
+				setIsBrowserClosed(true);
+			}else {
+				Log.info("Driver instance is closed already");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+	}
+	
+
+	
 }
